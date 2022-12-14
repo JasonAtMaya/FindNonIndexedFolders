5572a7b5565ab2ee6849c91d9cbb3e610feb0186
JasonAtMaya <jason.titcomb@mayahtt.com>
2022-12-14 09:59:51 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace FindNonIndexedFolders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.btnTestQRY = new System.Windows.Forms.Button();
+            this.txtQry = new System.Windows.Forms.TextBox();
+            this.txtVault = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(675, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStatus.Location = new System.Drawing.Point(12, 459);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(776, 0);
+            this.txtStatus.TabIndex = 1;
+            this.txtStatus.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 422);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vault";
+            this.label1.Visible = false;
+            // 
+            // txtResults
+            // 
+            this.txtResults.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResults.Location = new System.Drawing.Point(15, 157);
+            this.txtResults.Multiline = true;
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(773, 156);
+            this.txtResults.TabIndex = 5;
+            // 
+            // btnTestQRY
+            // 
+            this.btnTestQRY.Location = new System.Drawing.Point(496, 331);
+            this.btnTestQRY.Name = "btnTestQRY";
+            this.btnTestQRY.Size = new System.Drawing.Size(136, 31);
+            this.btnTestQRY.TabIndex = 6;
+            this.btnTestQRY.Text = "Test QRY";
+            this.btnTestQRY.UseVisualStyleBackColor = true;
+            this.btnTestQRY.Click += new System.EventHandler(this.btnTestQRY_Click);
+            // 
+            // txtQry
+            // 
+            this.txtQry.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtQry.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::FindNonIndexedFolders.Properties.Settings.Default, "QRY", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtQry.Location = new System.Drawing.Point(15, 33);
+            this.txtQry.Multiline = true;
+            this.txtQry.Name = "txtQry";
+            this.txtQry.Size = new System.Drawing.Size(773, 118);
+            this.txtQry.TabIndex = 4;
+            this.txtQry.Text = global::FindNonIndexedFolders.Properties.Settings.Default.QRY;
+            // 
+            // txtVault
+            // 
+            this.txtVault.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtVault.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::FindNonIndexedFolders.Properties.Settings.Default, "vault", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.txtVault.Location = new System.Drawing.Point(64, 422);
+            this.txtVault.Name = "txtVault";
+            this.txtVault.Size = new System.Drawing.Size(724, 22);
+            this.txtVault.TabIndex = 2;
+            this.txtVault.Text = global::FindNonIndexedFolders.Properties.Settings.Default.vault;
+            this.txtVault.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 378);
+            this.Controls.Add(this.btnTestQRY);
+            this.Controls.Add(this.txtResults);
+            this.Controls.Add(this.txtQry);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtVault);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "BIDB";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.TextBox txtVault;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQry;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Button btnTestQRY;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindNonIndexedFolders
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var vaultPath = txtVault.Text;
+            if (Directory.Exists(vaultPath))
+            {
+                txtStatus.Text = "Begin" + Environment.NewLine;
+                ValidateDirectoryAttrib(vaultPath);
+                txtStatus.AppendText("End");
+            }
+            else
+            {
+                txtStatus.Text = vaultPath + " does not exist.";
+            }
+        }
+
+
+        public void ValidateDirectoryAttrib(string directory)
+        {
+            ValidateDirectoryAttrib(new DirectoryInfo(directory));
+        }
+
+        private void ValidateDirectoryAttrib(DirectoryInfo directory)
+        {
+            if ((directory.Attributes & FileAttributes.NotContentIndexed) == FileAttributes.NotContentIndexed)
+            {
+                txtStatus.AppendText(directory.FullName + Environment.NewLine);
+            }
+            try
+            {
+                DirectoryInfo[] directories = directory.GetDirectories();
+                DirectoryInfo[] array = directories;
+                foreach (DirectoryInfo dirs in array)
+                {
+                    ValidateDirectoryAttrib(dirs);
+                }
+            }
+            catch (Exception)
+            {
+
+                txtStatus.AppendText("ERR:" + directory.FullName + Environment.NewLine);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtVault.Text = Properties.Settings.Default.vault;
+            //GetDefaultQuery();
+        }
+
+        private void GetDefaultQuery()
+        {
+            string empty = string.Empty;
+            string text = "D:\\MyVault\\00000002_A.par";
+            string text2 = "SystemIndex";
+            string text3 = "System.ItemUrl";
+            string empty2 = string.Empty;
+            empty = "SELECT";
+            empty += " ";
+            empty += "SolidEdge.DocumentNumber";
+            empty += " ";
+            empty += "FROM ";
+            Uri uri = new Uri(text);
+            empty2 = ((!uri.IsUnc) ? (empty2 + text2) : (empty2 + uri.Host + "." + text2));
+            empty += empty2;
+            empty += " ";
+            empty += "WHERE";
+            empty += " ";
+            empty += text3;
+            empty += "=";
+            empty = empty + "'" + text + "'";
+            txtQry.Text = "SELECT System.ItemUrl FROM SystemIndex WHERE SolidEdge.DocumentNumber='MM-00177-001'";
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.vault = txtVault.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnTestQRY_Click(object sender, EventArgs e)
+        {
+            OleDbDataReader oleDbDataReader = null;
+            OleDbConnection oleDbConnection = null;
+            OleDbCommand oleDbCommand = null;
+
+            try
+            {
+                txtResults.Clear();
+                string qry = txtQry.Text;
+                if (string.IsNullOrEmpty(qry))
+                {
+                    return;
+                }
+                if (qry.Contains("System.ItemUrl="))
+                {
+                    string text2 = qry.Substring(qry.IndexOf("System.ItemUrl=") + 15);
+                    char[] separator = new char[1] { '\'' };
+                    text2 = text2.Split(separator)[1];
+                    if (!File.Exists(text2))
+                    {
+                        txtResults.Text += Environment.NewLine + "File does not exists!";
+                        return;
+                    }
+                }
+                oleDbConnection = new OleDbConnection("Provider=Search.CollatorDSO;Extended Properties=\"Application=Windows\"");
+                oleDbCommand = new OleDbCommand(qry, oleDbConnection);
+                oleDbConnection.Open();
+                oleDbDataReader = oleDbCommand.ExecuteReader();
+                if (!oleDbDataReader.HasRows)
+                {
+                    txtResults.Text += Environment.NewLine + "Indexing is not working";
+                }
+                else
+                {
+                    while (oleDbDataReader.Read())
+                    {
+                        txtResults.Text = txtResults.Text + Environment.NewLine + "HasRows= " + oleDbDataReader.HasRows + Environment.NewLine;
+                        for (int i = 0; i < oleDbDataReader.FieldCount; i++)
+                        {
+                            string name = oleDbDataReader.GetName(i);
+                            string propVal = oleDbDataReader.GetValue(i).ToString();
+                            txtResults.AppendText("Property Name: " + name + " = " + propVal + Environment.NewLine);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                txtResults.Text += "\nException occured:\n";
+                txtResults.Text += ex.Message;
+            }
+            finally
+            {
+                if (oleDbDataReader != null)
+                {
+                    oleDbDataReader.Close();
+                    oleDbDataReader.Dispose();
+                }
+                if (oleDbConnection!=null && oleDbConnection.State == ConnectionState.Open)
+                {
+                    oleDbConnection.Close();
+                }
+            }
+        }
+    }
+}
